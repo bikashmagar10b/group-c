a7f2167b2163d58e73edb11b2287c13bc9ebdbad
bikashmagar10b <139779376+bikashmagar10b@users.noreply.github.com>
2024-02-17 19:54:31 +0545

    dd

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+    $("#icon1").click(function(){
+    $("#cll").toggle();
+    })
+    })
+    
+    function logo(){
+        alert("Welcome to Paperwork.Feel free to Join Us.");
+    }
+    
