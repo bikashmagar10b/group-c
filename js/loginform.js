4c73a44b16336c95127f744e49cf80ca8db5093c
bikashmagar10b <139779376+bikashmagar10b@users.noreply.github.com>
2024-02-18 11:00:35 +0545

    dd

---
+++ js/loginform.js
@@ -0,0 +1,25 @@
+function abc()
+{
+    let a = document.getElementById('username').value;
+    let b = document.getElementById('password').value;
+
+    if(a=="groupc@gmail.com" && b=="groupc@123")
+    {
+        alert("Successful log In");
+        var datab = '../html/page1.html';
+        window.open(datab);
+    }
+    else{
+        alert("Database Error!");
+    }
+}
+
+function passw(){
+    let passw = document.getElementById('password');
+    if (passw.type === "password") {
+        passw.type = "text";
+    }
+     else {
+        passw.type = "password";
+    }
+}
