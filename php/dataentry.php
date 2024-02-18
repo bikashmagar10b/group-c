af5ef8efe3df62735d17a7e4a3f70a0790eb1e95
bikashmagar10b <139779376+bikashmagar10b@users.noreply.github.com>
2024-02-19 00:10:05 +0545

    ss

--- php/dataentry.php
+++ php/dataentry.php
@@ -20,6 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
+        header("Location: ../html/loginform.html");
+
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
