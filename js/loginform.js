3ed289ebfeb0031d286fa08de543554223cb16b6
bikashmagar10b <139779376+bikashmagar10b@users.noreply.github.com>
2024-02-18 23:51:20 +0545

    ss

--- js/loginform.js
+++ js/loginform.js
@@ -1,18 +1,18 @@
-function abc()
-{
-    let a = document.getElementById('username').value;
-    let b = document.getElementById('password').value;
+// function abc()
+// {
+//     let a = document.getElementById('username').value;
+//     let b = document.getElementById('password').value;
 
-    if(a=="groupc@gmail.com" && b=="groupc@123")
-    {
-        alert("Successful log In");
-        var datab = '../html/page1.html';
-        window.open(datab);
-    }
-    else{
-        alert("Wrong username or password!!");
-    }
-}
+//     if(a=="groupc@gmail.com" && b=="groupc@123")
+//     {
+//         alert("Successful log In");
+//         var datab = '../html/page1.html';
+//         window.open(datab);
+//     }
+//     else{
+//         alert("Wrong username or password!!");
+//     }
+// }
 
 function passw(){
     let passw = document.getElementById('password');
