263f0714c38008951f861692459b00e64068408a
bikashmagar10b <139779376+bikashmagar10b@users.noreply.github.com>
2024-02-18 21:35:13 +0545

    ee

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
@@ -10,5 +10,7 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+else{
 echo "Connected successfully";
+}
 ?>
